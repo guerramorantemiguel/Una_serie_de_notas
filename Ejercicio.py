a2f4f18556ba23251892bac0f3f09d440d32723b
miguelguerra <100090620+guerramorantemiguel@users.noreply.github.com>
2022-04-27 19:28:37 +0000

    código

--- Ejercicio.py
+++ Ejercicio.py
@@ -6,3 +6,7 @@ notas_lectura = list(notas["reading score percentage"])
 notas_escritura = list(notas["writing score percentage"])
 
 m = len(notas_mates)
+
+for i in range (1,m):
+  diccionario = {"notas_mates":notas_mates, "notas_lectura":notas_lectura, "notas_escritura":notas_escritura}
+  print(1)
