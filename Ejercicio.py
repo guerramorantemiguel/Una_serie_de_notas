335cccd443b78d19b53c66a115767e00ca2575c7
miguelguerra <100090620+guerramorantemiguel@users.noreply.github.com>
2022-04-27 19:25:27 +0000

    código

--- Ejercicio.py
+++ Ejercicio.py
@@ -1,22 +1,8 @@
-def calculoMediana(caracteristica):  
-   mediana = 0  
-   caracteristica = caracteristica.sort_values()  
-   caracteristica = caracteristica.reset_index(drop=True)  
-   n = self. caracteristica.count()  
-   par = False;  
-   if (n % 2 == 0):  
-       print("La cantidad de observaciones es par.")  
-       par = True  
-  
-   if par:  
-       rango = (n / 2); 
-       rangoPython = rango-1 
-       valor1 = caracteristica[rangoPython]  
-       valor2 = caracteristica[rangoPython+1]  
-       mediana = valor1 +((valor2-valor1)/2)  
-   else:  
-       rango = ((n + 1) / 2)  
-       rangoPython = rango - 1  
-       mediana = caracteristica [rangoPython]  
-  
-   return mediana 
+import pandas as pd
+
+notas = pd.read_csv("Student_Performance_new.csv", header = 0)
+notas_mates = list(notas["math percentage"])
+notas_lectura = list(notas["reading score percentage"])
+notas_escritura = list(notas["writing score percentage"])
+
+m = len(notas_mates)
